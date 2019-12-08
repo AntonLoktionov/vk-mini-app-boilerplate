fb7168de619cc94289dc6ed9ad29ef1934a6d09b
Antonio <antoniopin41@gmail.com>
2019-12-08 11:09:58 +0300

    Updates

---
+++ precache-manifest.b568d30f2b1c7be3c71dcbcb25a4cd05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bce334f206c0d45517c5",
+    "url": "/vk-mini-app-boilerplate/static/css/main.da5ec31d.chunk.css"
+  },
+  {
+    "revision": "bce334f206c0d45517c5",
+    "url": "/vk-mini-app-boilerplate/static/js/main.363eef96.chunk.js"
+  },
+  {
+    "revision": "c286cce7deafa53a7c4f",
+    "url": "/vk-mini-app-boilerplate/static/js/runtime~main.4e009b77.js"
+  },
+  {
+    "revision": "d4ac63207302b24129b1",
+    "url": "/vk-mini-app-boilerplate/static/css/2.afa90a00.chunk.css"
+  },
+  {
+    "revision": "d4ac63207302b24129b1",
+    "url": "/vk-mini-app-boilerplate/static/js/2.af833b1e.chunk.js"
+  },
+  {
+    "revision": "5b75e0a14ee82d143193a927b7931173",
+    "url": "/vk-mini-app-boilerplate/index.html"
+  }
+];
